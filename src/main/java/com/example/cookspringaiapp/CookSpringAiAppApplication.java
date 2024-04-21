e5ba3f104164d82715cacb2765e6bcb516d5f9b3
Rafal Stefanski <rafal-stefanski@wp.pl>
2024-04-21 15:40:59 +0200

    Initial commit (T)

---
+++ src/main/java/com/example/cookspringaiapp/CookSpringAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookspringaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookSpringAiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookSpringAiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookspringaiapp/CookSpringAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookspringaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookSpringAiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
